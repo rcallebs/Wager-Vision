6385a84aad413e072af6088f3ddf2108e96a894a
Ryan Callebs <ryan.callebs@gmail.com>
2024-05-21 10:10:17 -0400

    logos added to coin

--- src/components/SpinningCoin.jsx
+++ src/components/SpinningCoin.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import "../assets/styles/SpinningCoin.css";
 import nfl from "../assets/images/nfl.png";
 import mlb from "../assets/images/mlb.png";
+import pga from "../assets/images/pga.png";
+import falcons from "../assets/images/falcons.png";
+import uefa from "../assets/images/uefa.png";
 import sportsbettings from "../assets/images/sportsbetting.png";
 
 const SpinningCoin = () => {
-  const logos = [nfl, mlb, sportsbettings];
+  const logos = [nfl, mlb, pga, falcons, uefa, sportsbettings];
   const [currentLogo, setCurrentLogo] = useState(nfl);
   const [logoIndex, setLogoIndex] = useState(0);
 
@@ -23,7 +26,7 @@ const SpinningCoin = () => {
 
   return (
     <div className="spinning-coin">
-      <img src={currentLogo} alt="Spinning coin" />
+      <img src={currentLogo} alt="spinning-coin logos" />
     </div>
   );
 };
